062e02147042219884827c7ecb38af545ea1ea1d
Vyacheslav <asik2006@mail.ru>
2025-03-14 17:51:29 +0300

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import useFetch from "./useFetch";
+import useFetch from "./hooks/useFetch";
 import "./App.css";
+import { useState } from "react";
 
-export function Home() {
-  // const url = "http://localhost:7070/data";
-  const url = "http://localhost:7070/error";
-  // const url = "http://localhost:7070/loading";
+export function Home({ url }) {
   const { data, loading, error } = useFetch(url);
 
   console.log("data: ", data);
@@ -26,9 +24,31 @@ export function Home() {
 }
 
 function App() {
+  const [url, setUrl] = useState("http://localhost:7070/data");
+  const urlOk = "http://localhost:7070/data";
+  const urlErr = "http://localhost:7070/error";
+  const urlLoading = "http://localhost:7070/loading";
+
+  const handleOk = () => {
+    setUrl(urlOk);
+  };
+
+  const handleErr = () => {
+    setUrl(urlErr);
+  };
+
+  const handleLoading = () => {
+    setUrl(urlLoading);
+  };
+
   return (
     <>
-      <Home />
+      <div>
+        <button onClick={() => handleOk()}>OK</button>
+        <button onClick={() => handleErr()}>Error</button>
+        <button onClick={() => handleLoading()}>Loading</button>
+      </div>
+      <Home url={url} />
     </>
   );
 }
